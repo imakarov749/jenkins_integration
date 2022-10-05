39bad4e1630490ba4670eee1a9c0a4fdb4c03115
imak749 <ivan.makarov199716@gmail.com>
2022-10-05 16:26:56 +0300

    Добавил в код открытие в удаленном браузере

--- model/pages/registration_form.py
+++ model/pages/registration_form.py
@@ -3,19 +3,38 @@ from selene import be, have
 import os
 from selene.support.conditions import be
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-from model.controls import dropdown, modal
+from model.controls import modal, dropdown, app
 from utils import attach
 
 
 @allure.step('Выполняем предусловия для теста')
 def opened_and_configure_browser():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor='https://user1:1234@selenoid.autotests.cloud/wd/hub',
+        options=options)
+
+    browser.config.driver = driver
     browser.config.hold_browser_open = True
     browser.config.base_url = 'https://demoqa.com'
     browser.open('/automation-practice-form')
     browser.should(have.url('https://demoqa.com/automation-practice-form'))
     browser.element('.main-header').should(be.visible)
     browser.driver.set_window_size(1920, 1080)
+
+    app.close_app()
 
 
 @allure.step('Заполняем имя')
@@ -75,7 +94,7 @@ def set_subject(subject):
 
 @allure.step('Загружаем изображение')
 def upload_image():
-    file_path = os.path.abspath("../resources/photo_image.jpg")
+    file_path = os.path.abspath("./resources/photo_image.jpg")
     browser.element('#uploadPicture').send_keys(file_path)
 
 
